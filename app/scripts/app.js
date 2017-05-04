70d7a44e90e4e99a28feb82d927aa27d430a7b24
Matthew Messier <messier_matthew@yahoo.com>
2017-05-03 23:14:53 -0400

    added the first two portions of the wire fram with a facebook login button that will function as soon as we have a domain name

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+var telepic = angular.module('TelePic', ['ngRoute']);
+telepic.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('');
+}]);
+telepic.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.
+	when('/login', {
+		templateUrl: 'app/templates/login.html',
+		controller: 'LoginController'
+	}).
+	when('/home', {
+		templateUrl: 'app/templates/home.html',
+		controller: 'HomeController'
+	}).
+	when('/game', {
+		templateUrl: 'app/templates/game.html',
+		controller: 'GameController'
+	})
+}])
---
+++ app/scripts/controllers/LoginController.js
@@ -0,0 +1,41 @@
+angular.module('TelePic').controller('LoginController', [
+    '$scope',
+    function($scope) {
+        // Here we run a very simple test of the Graph API after login is
+        // successful.  See statusChangeCallback() for when this call is made.
+        function testAPI() {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function(response) {
+                console.log('Successful login for: ' + response.name);
+                document.getElementById('status').innerHTML =
+                    'Thanks for logging in, ' + response.name + '!';
+            });
+        }
+        function statusChangeCallback(response) {
+            console.log('statusChangeCallback');
+            console.log(response);
+            // The response object is returned with a status field that lets the
+            // app know the current login status of the person.
+            // Full docs on the response object can be found in the documentation
+            // for FB.getLoginStatus().
+            if (response.status === 'connected') {
+                // Logged into your app and Facebook.
+                testAPI();
+            } else {
+                // The person is not logged into your app or we are unable to tell.
+                document.getElementById('status').innerHTML = 'Please log ' +
+                    'into this app.';
+            }
+        }
+
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+        }
+
+        FB.logout(function(response) {
+            // Person is now logged out
+        });
+    }
+]);
