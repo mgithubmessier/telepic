cff3e40b2b269d8409c7494fb13374a61eef16e3
Matthew Messier <messier_matthew@yahoo.com>
2017-05-08 23:48:38 -0400

    added more to the game wireframe

--- app/scripts/controllers/GameController.js
+++ app/scripts/controllers/GameController.js
@@ -1,13 +1,52 @@
 angular.module('TelePic').controller('GameController', [
-    '$scope',
-    '$routeParams',
-    '$http',
-    function($scope, $routeParams, $http) {
-        console.log($routeParams.iGameId);
+    '$scope','$routeParams','$http','$location',
+    function($scope, $routeParams, $http, $location) {
+        var jsonGameMenu,jsonPlayersStatus,jsonRecMessage,jsonSenMessage;
+
+        function showGameMenu() {
+            $scope.bGameMenu = true;
+            $scope.bPlayersStatus = $scope.bRecMessage = $scope.bSenMessage = false;              
+        }
+        function showPlayerStatusMenu() {
+            $scope.bPlayersStatus = true;            
+            $scope.bGameMenu = $scope.bRecMessage = $scope.bSenMessage = false;
+        }
+        function showRecMessage() {
+            $scope.bRecMessage = true;
+            $scope.bPlayersStatus = $scope.bGameMenu = $scope.bSenMessage = false;
+        }
+        function showSenMessage() {
+            $scope.bSenMessage = true;
+            $scope.bPlayersStatus = $scope.bGameMenu =  $scope.bRecMessage = false;            
+        }
+        $scope.viewPlayersStatus = function() {
+            showPlayerStatusMenu();
+            $http.get('app/mock_data/players_status.json').then(function(data) {
+                jsonPlayersStatus = data.data;
+            });
+        }
+        $scope.viewGameMenu = function() {
+            showGameMenu();
+            $http.get('app/mock_data/game_menu.json').then(function(data) {
+                jsonGameMenu = data.data;
+            });            
+        }
+        $scope.viewRecMessage = function() {
+            showRecMessage();
+            $http.get('app/mock_data/rec_message.json').then(function(data) {
+                jsonRecMessage = data.data;
+            });
+        }
+        $scope.viewSenMessage = function() {
+            showSenMessage();
+            $http.get('app/mock_data/sen_message.json').then(function(data) {
+                jsonSenMessage = data.data;
+            });
+        }
+        showGameMenu();
         $scope.iSelectedGame = $routeParams.iGameId;
-        var jsonGames;
-        $http.get('app/mock_data/games.json').then(function(data) {
-            jsonGames = data.data;
-        });
+        $scope.sRecMessage = 'Received message';
+        $scope.sSenMessage = 'Sent message';
+        $scope.sPlayersStatus = 'View players and status';
     }
 ]);
